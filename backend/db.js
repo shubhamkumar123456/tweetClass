d2bf80cf3f828494bcc7c0c58801eb75b25f424c
shubham kumar <clboy768@gmail.com>
2024-11-16 13:54:08 +0530

    updated mongo atlas

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 const connectToDb = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/twitterApp')
+    // mongoose.connect('mongodb://127.0.0.1:27017/twitterApp')
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@tweetapp.mef5j.mongodb.net/?retryWrites=true&w=majority&appName=tweetApp`)
     .then(() => console.log('connect to mongodb successfully!'))
     .catch(()=>console.log('error in connecting mongodb'));
 }
